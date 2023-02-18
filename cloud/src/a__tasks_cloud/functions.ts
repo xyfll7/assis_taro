c408bc65f2c78ec808e141589245b5f2eb49a26e
xyfll7 <l.7@qq.com>
2023-02-18 10:26:53 +0800

    feat(client): 🎸 新疆西藏海南价格规则

--- cloud/src/a__tasks_cloud/functions.ts
+++ cloud/src/a__tasks_cloud/functions.ts
@@ -73,22 +73,49 @@ export async function getPrice_cloud(event: Events<Product_Express>) {
   return {
     code: Code.SUCCESS,
     message: "价格计算成功",
-    data: { ...data, totalFee: utils_calc_express_totalFee(data.weight) } as Product_Express,
+    data: { ...data, totalFee: utils_calc_express_totalFee(data) } as Product_Express,
   };
 }
 
 
 //#region 计算快递订单总价  // 1公斤6元、2公斤7元、3公斤8元 4公斤*2.5元
-export function utils_calc_express_totalFee(weight: number): number {
-  switch (weight) {
-    case 1:
-      return 6 * 100;
-    case 2:
-      return 7 * 100;
-    case 3:
-      return 8 * 100;
+export function utils_calc_express_totalFee(data: Product_Express): number {
+  const weight = data.weight;
+  const province = data.recMan!.province.trim();
+
+  // 新疆：首重15（12）+续重10（8）
+
+  // 海南：首重12（8）+续重5（3）
+
+  // 西藏：首重18（15）+续重12（10）
+
+  switch (province) {
+    case "新疆维吾尔自治区":
+      return price_1(weight, 15, 10);
+    case "海南省":
+      return price_1(weight, 12, 5);
+    case "西藏自治区":
+      return price_1(weight, 15, 12);
     default:
-      return weight * 2.5 * 100;
+      return price_0(weight);
   }
+
+  function price_1(_weight: number, first: number, plus: number) {
+    return (_weight * plus + (first - plus)) * 100;
+  };
+
+  function price_0(_weight: number) {
+    switch (_weight) {
+      case 1:
+        return 6 * 100;
+      case 2:
+        return 7 * 100;
+      case 3:
+        return 8 * 100;
+      default:
+        return _weight * 2.5 * 100;
+    }
+  }
+
 }
 //#endregion
